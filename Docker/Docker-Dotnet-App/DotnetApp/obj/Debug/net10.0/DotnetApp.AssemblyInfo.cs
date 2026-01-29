7d3122e249333542d128976de40fb4ecb0f56e56
Murali Kuruba <muralikuruba11@gmail.com>
2026-01-29 09:11:47 +0530

    changesfordontnetapp

--- Docker/Docker-Dotnet-App/DotnetApp/obj/Debug/net10.0/DotnetApp.AssemblyInfo.cs
+++ Docker/Docker-Dotnet-App/DotnetApp/obj/Debug/net10.0/DotnetApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotnetApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e40dcfa66c8aa1394a259ca1d70d2fa03599ba58")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+591a6ace321031d9614301887e44d473db006671")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotnetApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotnetApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
